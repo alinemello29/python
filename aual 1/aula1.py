da7609049a0bf03cb52f75be0afe429165f90392
Aline Melo <109696840+alinemello29@users.noreply.github.com>
2022-08-23 11:24:50 -0300

    Add files via upload

---
+++ aual 1/aula1.py
@@ -0,0 +1,50 @@
+ Príncipios de pensamentos compuatacionais com python
+ Pensamento compuatcional
+ resolver prblema de forma assertiva 
+ o que é? 
+
+ Padagobot, o que é pensamento compuatcional 
+ se nem você que é robô sabe, imagina só os humanos. Mas vou explicar.
+ 
+ NÃO é saber mexer no computador/smartphone.
+ NÃO é preciso usar o compuatador para entender.
+ NÃO é só paar quem é ou que ser da área da computação.
+
+ O que é ?
+ "São os processos de pensamentos envolvidos na formulação de um problema e que expressam sua solução ou soluções eficazmente, de tal forma que uma máquina ou uma pessoa possam realizar"
+ "processo de reconhecer aspecto da computação em um mundo que nos cerca e, técnicas da ciência da computação para entender e argumentar sobre sistemas e processos naturaias e artificias"
+ 
+ Os 4 pilares   
+ abstração, decomposição, pensamento algoritmo, identificaçaõ de padrões 
+   
+   qual a importância 
+   Por que praticar?
+Letramento digiatl e mundo digitalizado
+resolução de problema e tomadas assertivas de decisão 
+bncc e técnos- pedagogia
+bônus pra quem programa
+ 
+ Porque aprender python
+ O que é 
+ Linguagem de programação de proósito geral 
+ python é a linguagem que mais cresceu nós últimos cincoanos, segundo o ýpl do github.
+ curva de aprendizado altamente escalável acesso e adição de biblioteca grande comunidade.
+
+ Abstração
+ Definição e exemplos 
+ O que é 
+ Ação de ignorar os detalhes de uam solução de modo que ela sera válida para diversos problemas
+ é isolar aspectos relevantes de um todo e considerá-los de forma individual
+ na computação quando pensamos em criar um software pensamos primeiro no que ele deve ser deixando os detalhes para depois 
+
+ abstração
+ Exemplo 
+ Uma diretora decide fazer um programa para saber qual a média geral dos alunos da escola 
+ o método utilizado por cada professor pode ser diferente mas todos os métodos resultam em uma nota final.     
+ a nota dos alunos é uma abstração
+ podemos não saber exatamente qual o método utilizado para conseguir a nota mas abstraimos isso e extraimos a informação mais importante: a nota  
+
+ Dica      REPL.IT  
+ Interpretador online para várias linguagem 
+ acesso rápido e possibilidade de adição de funcionalidades
+ boa performance. 
