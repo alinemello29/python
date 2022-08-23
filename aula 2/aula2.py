84f12373ad301d8847681c140077de1aa99e2814
Aline Melo <109696840+alinemello29@users.noreply.github.com>
2022-08-23 13:19:13 -0300

    Add files via upload

---
+++ aula 2/aula2.py
@@ -0,0 +1,13 @@
+ensamentos Algorítmico  
+ Definiçãoe exemplos 
+ O que é 
+ Ação de pessoas a solução de uma problema a partir de uma sequência dinita de passos 
+ na computação quando escrevemos um código descrevemos passo-a-passo o que aquele código deve fazer 
+ se o código não resolve o probelma é possível checar onde está o erro fazendo um teste de  mesa 
+ 
+
+ pensamentos algoritmico 
+ exemplo 
+ você está em um supermercado e tem apenas R$20.00 e decide dividir igaulmente o valor que pretende gastar para comprar 2 itens 
+ é possível resolver o problema  de vários formas 
+ uma das soluções mais básicas usando uma alta abstração: é (1) é preciso saber o valor do produto (2) é preciso comparar o valor do produto como o valor disponível para gastar um cada produto
---
+++ aula 2/exemplo aula2.py
@@ -0,0 +1,7 @@
+arroz_kg = 7.00
+feijao_kg =  9.00
+carne_kg = 35.00
+if arroz_kg <= 10.00:
+  print("posso comprar!")
+else: 
+  print("não posso comprar!")
